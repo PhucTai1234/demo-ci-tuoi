6b80e84f9c5766d9081632a2c5942e0f0fcf9c2e
PhucTai1234 <1822040488@dntu.edu.vn>
2025-04-17 18:33:24 +0700

    Update KiemTraTuoi.test.js

--- kiemtra/KiemTraTuoi.test.js
+++ kiemtra/KiemTraTuoi.test.js
@@ -2,7 +2,7 @@ const { tinhTuoi, laNguoiLon } = require('../TinhTuoi');
 
 describe('Kiểm tra tuổi', () => {
   test('tính tuổi cho người sinh năm 2000', () => {
-    expect(tinhTuoi(2000)).toBe(25);
+    expect(tinhTuoi(2000)).toBe(26);
   });
 
   test('tính tuổi cho người sinh năm 2010', () => {
