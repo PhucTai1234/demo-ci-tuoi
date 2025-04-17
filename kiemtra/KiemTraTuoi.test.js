d6a4071fdccbe10e5dca8a8241de0f5d52dca9c2
PhucTai1234 <1822040488@dntu.edu.vn>
2025-04-17 17:07:37 +0700

    Thêm hàm laNguoiLon va tien hanh kiem thu

--- kiemtra/KiemTraTuoi.test.js
+++ kiemtra/KiemTraTuoi.test.js
@@ -1,4 +1,4 @@
-const { tinhTuoi } = require('../TinhTuoi');
+const { tinhTuoi, laNguoiLon } = require('../TinhTuoi');
 
 describe('Kiểm tra tuổi', () => {
   test('tính tuổi cho người sinh năm 2000', () => {
@@ -7,5 +7,9 @@ describe('Kiểm tra tuổi', () => {
 
   test('tính tuổi cho người sinh năm 2010', () => {
     expect(tinhTuoi(2010)).toBe(15);
+  });
+
+  test('kiểm tra người sinh năm 2000 là người lớn', () => {
+    expect(laNguoiLon(2000)).toBe(true);
   });
 });
